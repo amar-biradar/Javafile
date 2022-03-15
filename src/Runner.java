a478fc4ef60047ce5137168719e99b7f02c664e5
amar <innocentboyamar@gmail.com>
2022-03-15 10:38:46 +0530

    data strusture

---
+++ src/Runner.java
@@ -0,0 +1,20 @@
+import java.
+public class Runner {
+
+	public static void main(String[] args) {
+		 
+		Linkedlist list = new Linkedlist();
+		list.insert(12);
+		list.insert(13);
+		list.insert(24);
+		list.insert(30);
+		
+		
+		list.show();
+		
+		
+		
+
+	}
+
+}
